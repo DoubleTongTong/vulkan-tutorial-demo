b26bd00484a477eb0cb2cad929b9d7f258f3ee09
tim <timchen021@qq.com>
2026-02-13 12:30:15 +0800

    index buffers and staging buffers
    Signed-off-by: tim <timchen021@qq.com>

--- VulkanCourseApp/Mesh.h
+++ VulkanCourseApp/Mesh.h
@@ -11,21 +11,31 @@ class Mesh
 {
 public:
 	Mesh();
-	Mesh(VkPhysicalDevice newPhysicalDevice, VkDevice newDevice, std::vector<Vertex>* vertices);
+	Mesh(VkPhysicalDevice newPhysicalDevice, VkDevice newDevice,
+		VkQueue transferQueue, VkCommandPool transferCommandPool,
+		std::vector<Vertex>* vertices, std::vector<uint32_t>* indices);
 
 	int getVertexCount();
 	VkBuffer getVertexBuffer();
-	void destroyVertexBuffer();
+
+	int getIndexCount();
+	VkBuffer getIndexBuffer();
+
+	void destroyBuffers();
 
 private:
 	int vertexCount;
 	VkBuffer vertexBuffer;
 	VkDeviceMemory vertexBufferMemory;
 
+	int indexCount;
+	VkBuffer indexBuffer;
+	VkDeviceMemory indexBufferMemory;
+
 	VkPhysicalDevice physicalDevice;
 	VkDevice device;
 
-	void createVertexBuffer(std::vector<Vertex>* vertices);
-	uint32_t findMemoryTypeIndex(uint32_t allowedTypes, VkMemoryPropertyFlags properties);
+	void createVertexBuffer(VkQueue transferQueue, VkCommandPool transferCommandPool, std::vector<Vertex>* vertices);
+	void createIndexBuffer(VkQueue transferQueue, VkCommandPool transferCommandPool, std::vector<uint32_t>* indices);
 };
 
