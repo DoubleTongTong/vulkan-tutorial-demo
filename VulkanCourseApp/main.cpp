fcabbd303cd26bd1f71cf70a47a11c17e610cbe1
tim <timchen021@qq.com>
2026-03-22 19:07:16 +0800

    depth buffer
    Signed-off-by: tim <timchen021@qq.com>

--- VulkanCourseApp/main.cpp
+++ VulkanCourseApp/main.cpp
@@ -1,3 +1,5 @@
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -52,7 +54,7 @@ int main()
 		firstModel = glm::translate(firstModel, glm::vec3(0.0f, 0.0f, -2.0f));
 		firstModel = glm::rotate(firstModel, glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
 		secondModel = glm::translate(secondModel, glm::vec3(0.0f, 0.0f, -3.0f));
-		secondModel = glm::rotate(secondModel, glm::radians(-angle*100), glm::vec3(0.0f, 0.0f, 1.0f));
+		secondModel = glm::rotate(secondModel, glm::radians(-angle*10), glm::vec3(0.0f, 0.0f, 1.0f));
 
 		vulkanRenderer.updateModel(0, firstModel);
 		vulkanRenderer.updateModel(1, secondModel);
