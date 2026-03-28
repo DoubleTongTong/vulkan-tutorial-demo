b1af63915b9ab57ce3e00566a2344be9835e6dcd
tim <timchen021@qq.com>
2026-03-28 21:07:49 +0800

    image setup and pipeline barriers
    Signed-off-by: tim <timchen021@qq.com>

--- VulkanCourseApp/VulkanRenderer.h
+++ VulkanCourseApp/VulkanRenderer.h
@@ -13,6 +13,8 @@
 #include <algorithm>
 #include <array>
 
+#include "stb_image.h"
+
 #include "Mesh.h"
 #include "Utilities.h"
 
@@ -78,6 +80,11 @@ private:
 	// VkDeviceSize minUniformBufferOffset;
 	// size_t modelUniformAlignment;
 	// UboModel* modelTransferSpace;
+
+	// - Assets
+	std::vector<VkImage> textureImages;
+	std::vector<VkDeviceMemory> textureImageMemory;
+
 
 	// - Pipeline
 	VkPipeline graphicsPipeline;
@@ -155,5 +162,10 @@ private:
 		VkMemoryPropertyFlags propFlags, VkDeviceMemory* imageMemory);
 	VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
 	VkShaderModule createShaderModule(const std::vector<char>& code);
+
+	int createTexture(std::string fileName);
+
+	// -- Loader Functions
+	stbi_uc* loadTextureFile(std::string fileName, int* width, int* height, VkDeviceSize* imageSize);
 };
 
